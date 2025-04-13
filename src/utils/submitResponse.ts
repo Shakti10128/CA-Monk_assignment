a91480b99837bb6baae0e75d6fddc045acd5ad10
shakti <kumarshakti2017@gmail.com>
2025-04-13 17:41:19 +0530

    remove the console from utils file

--- src/utils/submitResponse.ts
+++ src/utils/submitResponse.ts
@@ -1,20 +1,11 @@
-
-
 const submitResponse = (selectedOptions:string[])=>{
         const storedResponses = sessionStorage.getItem("user-response");
-
         // Retrieve existing responses or initialize an empty array if not present
         const responses: string[][] = storedResponses ? JSON.parse(storedResponses) : [];
-        
         // Current response being added (selectedOptions is an array of strings)
         const currResponse: string[] = selectedOptions;
-        
         // Push the current response into the responses array
         responses.push(currResponse);
-        
-        // Log the responses to see the structure
-        console.log(responses);
-        
         // Save the updated responses array back into sessionStorage as a JSON string
         sessionStorage.setItem("user-response", JSON.stringify(responses));
 }
